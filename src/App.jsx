880952bfeb84185ce604caf4ec62d275d8f7a109
Muhammad Mudassir Zaman <127381696+zamanmudassir200@users.noreply.github.com>
2024-07-04 02:28:49 +0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Favorites from "./pages/favorites/Favorites";
+import Details from "./pages/details/Details";
+import Home from "./pages/home/Home";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="">
+        <div className="min-h-screen p-6 bg-white text-gray-600 text-lg">
+          <Navbar />
+          <Routes>
+            <Route to="/" element={<Home />} />
+            <Route to="/favorites" element={<Favorites />} />
+            <Route to="/recipe-item/:id" element={<Details />} />
+          </Routes>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;

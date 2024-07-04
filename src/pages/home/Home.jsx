ed72ae78c67199d0321056eb059a366bd145ff94
Muhammad Mudassir Zaman <127381696+zamanmudassir200@users.noreply.github.com>
2024-07-04 23:13:09 +0500

    final commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,8 +6,9 @@ const Home = () => {
   const { loading, recipeList, errMsg } = useContext(GlobalContext);
   if (loading) {
     return (
-      <div className="text-center">
-        <h1 className="font-bold text-xl ">Loading please wait</h1>;
+      <div className="text-center flex flex-col gap-2 h-[calc(100vh-200px)] items-center justify-center mt-4 ">
+        <h1 className="font-bold text-xl ">Loading! please wait...</h1>
+        <div className="h-[60px] w-[60px] rounded-full border-t-8 border-b-8 border-red-500 animate-spin"></div>
       </div>
     );
   }
@@ -17,7 +18,7 @@ const Home = () => {
         recipeList.map((item) => <RecipeItem item={item} key={item.id} />)
       ) : (
         <p className="font-bold text-2xl ">
-          Nothing to show! Please search something else
+          Nothing to show! Please search something
         </p>
       )}
     </div>
